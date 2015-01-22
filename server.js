1031512c626ea0eae1d379f140edace4cba4860d
Luiz Sócrate <luiz@socrate.com.br>
2015-01-21 23:45:56 -0200

    Setup morgan

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 "use strict"
 
-var express = require("express")
 var bodyParser = require("body-parser")
 var compression = require("compression")
 var controllers = require("./controllers")
+var express = require("express")
+var morgan = require("morgan")
+
+var env = process.env.NODE_ENV || "development"
 
 var app = express()
 
 app.use(compression())
 app.use(bodyParser.json())
+
+if (env === "development") app.use(morgan("dev"))
+else if (env === "production") app.use(morgan("combined", { skip: successes }))
+
+function successes (req, res) {
+  return res.statusCode < 400
+}
 
 controllers.initializeRoutes(app, "/api/v2")
   .then(function () {
