637b413b560ea460b368b4f69361385b7fb25046
Luiz Sócrate <luiz@socrate.com.br>
2015-05-15 20:30:42 -0300

    Uses graphene instead of graph story

--- libs/db.js
+++ libs/db.js
@@ -5,6 +5,6 @@ var env = process.env.NODE_ENV || "development"
 var config = require(__dirname + "/../config")[env]
 var Neo4j = require("node-neo4j")
 
-var dbURL = env === "production" ? process.env.GRAPHSTORY_URL : config.db.server
+var dbURL = env === "production" ? process.env.GRAPHENEDB_URL : config.db.server
 
 module.exports = Promise.promisifyAll(new Neo4j(dbURL))
