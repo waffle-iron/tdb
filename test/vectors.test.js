12b153e0770e322c199da5703e97adbe44e50954
Luiz Sócrate <luiz@socrate.com.br>
2015-06-18 13:14:36 -0300

    Add rest of CRUD tests for vectors

--- test/vectors.test.js
+++ test/vectors.test.js
@@ -75,4 +75,78 @@ describe("/api/v2/vectors resource", function () {
         .then(Test.returnModels)
     })
   })
+
+  describe("GET /vectors/:vector_id", function () {
+    var vector
+    before(function () {
+      return api
+        .post(url("vectors")).send({ vector: random.vector() })
+        .set("Authorization", authorization)
+        .expect(201).endAsync()
+        .then(function (res) { vector = res.body.vector })
+    })
+
+    it("Get a vector by id", function () {
+      return api
+        .get(url("vectors", vector.id))
+        .expect(200).endAsync()
+        .then(Test.returnModel)
+        .then(Test.haveOnlyModelProperties)
+    })
+  })
+
+  describe("PUT /vectors/:vector_id", function () {
+    var vector
+    before(function () {
+      return api
+        .post(url("vectors")).send({ vector: random.vector() })
+        .set("Authorization", authorization)
+        .expect(201).endAsync()
+        .then(function (res) { vector = res.body.vector })
+    })
+
+    it("Update a vector", function () {
+      vector.question = `Do ${vector.question}`
+      return api
+        .put(url("vectors", vector.id))
+        .send({ vector: vector })
+        .set("Authorization", authorization)
+        .expect(200).endAsync()
+        .then(Test.returnModel)
+        .then(Test.haveOnlyModelProperties)
+    })
+
+    it("Denies unauthenticated vector update", function () {
+      vector.question = `Do ${vector.question}`
+      return api
+        .put(url("vectors", vector.id))
+        .send({ vector: vector })
+        .expect(401).endAsync()
+    })
+  })
+
+  describe("DELETE /vectors/:vector_id", function () {
+    var vector
+    before(function () {
+      return api
+        .post(url("vectors")).send({ vector: random.vector() })
+        .set("Authorization", authorization)
+        .expect(201).endAsync()
+        .then(function (res) { vector = res.body.vector })
+    })
+
+    it("Delete a vector", function () {
+      return api
+        .delete(url("vectors", vector.id))
+        .set("Authorization", authorization)
+        .expect(204).endAsync()
+        .then(Test.responseEmpty)
+    })
+
+    it("Denies unauthorized vector delete", function () {
+      return api
+        .delete(url("vectors", vector.id))
+        .expect(401).endAsync()
+    })
+  })
 })
