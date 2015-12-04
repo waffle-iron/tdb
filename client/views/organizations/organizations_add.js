9e8b68448379ff39067695ac6ca2c766b1a41e1e
Lucas N. Munhoz <ln.munhoz@gmail.com>
2015-12-04 02:00:56 -0200

    [organization] Template, method and schema

--- client/views/organizations/organizations_add.js
+++ client/views/organizations/organizations_add.js
@@ -1,20 +1,31 @@
-Template.organizationsAdd.helpers({
-  steps: function() {
-    return [
-      {
-        id: 'information',
-        title: 'Organization Information',
-        schema: Schemas.Organization
+AutoForm.hooks({
+  insertOrganizationForm: {
+    onSuccess() {
+        toastr.success('Organization created successfully: ' + this.insertDoc.name, 'Success');
+        FlowRouter.go('organizations.index');
       },
-      {
-        id: 'keyPeople',
-        title: 'Organization Key People',
-        schema: Schemas.KeyPeople,
-        onSubmit: function(data, wizard){
-          console.log (data);
-          console.log (wizard);
-        }
-      }
-    ];
+      onError(formType, error) {
+        toastr.success(error.toString(), 'Error');
+      },
   }
 });
+
+
+  // steps: function() {
+  //   return [
+  //     {
+  //       id: 'information',
+  //       title: 'Organization Information',
+  //       schema: Schemas.Organization
+  //     },
+  //     {
+  //       id: 'keyPeople',
+  //       title: 'Organization Key People',
+  //       schema: Schemas.KeyPeople,
+  //       onSubmit: function(data, wizard){
+  //         console.log (data);
+  //         console.log (wizard);
+  //       }
+  //     }
+  //   ];
+  // }
