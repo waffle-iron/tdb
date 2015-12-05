b2f67db40bc24a82395916598faf49cad062ba46
shact.developer@gmail.com <shact.developer@gmail.com>
2015-12-04 22:50:44 -0200

    organizations table fix & removed elastic search

--- client/views/organizations/organizations_index.js
+++ client/views/organizations/organizations_index.js
@@ -1,22 +1,3 @@
 Template.organizations.onCreated(function() {
-  TabularTables.Organizations = new Tabular.Table({
-    name: 'OrganizationsList',
-    collection: Organizations,
-    sub: new SubsManager(),
-    pub: 'tabularOrganizationsList',
-    autoWidth: false,
-    responsive: true,
-    stateSave: true,
-    extraFields: ['_id'],
-    columns: [{
-      title: 'Name',
-      data: 'name',
-      width: '90%'
-  }, {
-      title: 'Actions',
-      tmpl: Template.organizationsActions,
-      width: '10%'
-  }],
-    bPaginate: true
-  });
+
 });
