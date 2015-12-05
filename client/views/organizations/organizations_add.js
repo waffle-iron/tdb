2be39f9ea0713f333f575dc98ae55741e27bd8ee
Lucas N. Munhoz <ln.munhoz@gmail.com>
2015-12-04 23:43:25 -0200

    Remove comments

--- client/views/organizations/organizations_add.js
+++ client/views/organizations/organizations_add.js
@@ -9,23 +9,3 @@ AutoForm.hooks({
       },
   }
 });
-
-
-  // steps: function() {
-  //   return [
-  //     {
-  //       id: 'information',
-  //       title: 'Organization Information',
-  //       schema: Schemas.Organization
-  //     },
-  //     {
-  //       id: 'keyPeople',
-  //       title: 'Organization Key People',
-  //       schema: Schemas.KeyPeople,
-  //       onSubmit: function(data, wizard){
-  //         console.log (data);
-  //         console.log (wizard);
-  //       }
-  //     }
-  //   ];
-  // }
