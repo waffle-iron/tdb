40b12f4867de0bb68ef56578c40bf8d7713b032f
Lucas N Munhoz <ln.munhoz@gmail.com>
2016-02-06 01:11:12 -0200

    Build attachment publication to get related entities

--- server/publications/attachments_publications.js
+++ server/publications/attachments_publications.js
@@ -19,11 +19,46 @@ Meteor.publishComposite('tabularAttachmentsList', function(tableName, ids, field
 
 Meteor.publishComposite('attachments.single', function(attachmentId) {
   check(attachmentId, String);
+  this.unblock();
+
+  let childQuery = {
+    attachmentsId: {
+      $in: [attachmentId]
+    }
+  };
+
+  let childOptions = {
+    fields: {
+      name: true  
+    }
+  };
+
   return {
     find() {
+      this.unblock();
       return Attachments.find({
         _id: attachmentId
       });
-    }
-  };
+    },
+    children: [{
+        find(attachment) {
+          return Meteor.users.find({
+            _id: attachment.createdBy
+          });
+        }
+      }, {
+        find(attachment) {
+          return Projects.find(childQuery, childOptions)
+        }
+      }, {
+        find(attachment) {
+          return Technologies.find(childQuery, childOptions)
+        },
+      }, {
+        find(attachment) {
+          return Organizations.find(childQuery, childOptions);
+        }
+      }
+    ]
+  }
 });
