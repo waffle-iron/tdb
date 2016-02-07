e6cc4c592511c1d8ca2bf41d620e1f88bf654bca
Lucas N Munhoz <ln.munhoz@gmail.com>
2016-02-07 17:52:47 -0200

    Add reactive context to attachmentsAdd

--- client/views/attachments/attachments_add.js
+++ client/views/attachments/attachments_add.js
@@ -1,12 +1,36 @@
 Template.attachmentsAdd.events({
-  'change input[name="url"]': function(e, t) {
+  'keyup #attachment-url': function(e, t) {
     let url = e.target.value;
     
     Meteor.call('getMetadataFromUrl', url, function(err, res) {
-      console.log(res);  
-
+      if (err){
+      	console.log('Error fetching metadata...')
+      }else {
+      	t.title.set(res.title);
+      	t.description.set(res.description);
+      	t.image.set(res.image);
+      }
     });
   }
+});
+
+Template.attachmentsAdd.helpers({
+  imageUrl: function(){
+  	return Template.instance().image.get();
+  }
+})
+
+Template.attachmentsAdd.onCreated(function(){
+  this.title = new ReactiveVar();
+  this.description = new ReactiveVar();
+  this.image = new ReactiveVar();
+
+  this.autorun(() => {
+  	$('input[name="title"]').val(this.title.get());
+  	$('input[name="description"]').val(this.description.get());
+  	$('input[name="imageUrl"]').val(this.image.get());
+  });
+
 });
 
 
