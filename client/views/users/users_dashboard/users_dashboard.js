0825f9bbbde734b522dc535c225c30fd56150b4c
Lucas N Munhoz <ln.munhoz@gmail.com>
2016-02-10 22:23:48 -0200

    Fix user table handle click event

--- client/views/users/users_dashboard/users_dashboard.js
+++ client/views/users/users_dashboard/users_dashboard.js
@@ -1,7 +1,7 @@
 Template.usersDashboard.events({
   'click tbody > tr': function(event) {
     handleTableClick(event, (rowData) => {
-      FlowRouter.go('usersEntry', {id: rowData._id});
+      FlowRouter.go('users.entry', {id: rowData._id});
     });
   }
 });
