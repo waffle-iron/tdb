9dac2ce9fe791a4ea9a84dbf4696b0b1f04ca680
lnmunhoz <ln.munhoz@gmail.com>
2016-02-16 16:15:24 -0200

    [attch] Prototyping attachment upload from remote url

--- client/views/attachments/attachments_add.js
+++ client/views/attachments/attachments_add.js
@@ -1,10 +1,41 @@
 Template.attachmentsAdd.events({
-  'input input[name="imageUrl"]': function(e, t){
+  'input input[name="imageUrl"]': function(e, t) {
     let attachment = t.attachment.get();
     attachment.imageUrl = e.target.value;
     t.attachment.set(attachment);
+  },
+
+  'click .btn-download': function(e, t) {
+    let url = $('#search-file-url').val();
+    console.log('File url:', url);
+
+    let newFile = new FS.File();
+    newFile.attachData(url, function(err) {
+      if (err) {
+        switch (err.error) {
+          case 500:
+            console.log('Could not read remote data from url. HEAD request is not allowed');
+          default:
+            console.log(err);
+        }
+      } else {
+        Meteor.call('uploadFileFromUrl', url, function(error, fileObj){
+          if (error){
+            console.log('Error uploading file:', error)
+          } else{
+            console.log('fileObj:', fileObj);
+            let attachment = t.attachment.get();
+
+            attachment.name = fileObj.name;
+            attachment.fileId = fileObj._id,
+
+            t.attachment.set(attachment);
+          }
+        });
+      }
+    });
   }
-})
+});
 
 Template.attachmentsAdd.helpers({
   attachment() {
@@ -33,6 +64,16 @@ Template.attachmentsAdd.onCreated(function() {
 AutoForm.hooks({
   insertAttachmentForm: {
     onSuccess() {
+      toastr.success('Attachment created successfully: ' + this.insertDoc.name, 'Success');
+      FlowRouter.go('attachments.dashboard');
+    },
+    onError(formType, error) {
+      toastr.error(error.toString(), 'Error');
+    },
+  },
+
+  insertAttachmentDownloadForm: {
+     onSuccess() {
       toastr.success('Attachment created successfully: ' + this.insertDoc.name, 'Success');
       FlowRouter.go('attachments.dashboard');
     },
