c520aa8b629eee35bddc0fd8ff3b2acf97d07c87
lnmunhoz <ln.munhoz@gmail.com>
2016-02-17 18:28:35 -0200

    [attach] Create a smart input file component

--- client/views/attachments/attachments_add_from_upload.js
+++ client/views/attachments/attachments_add_from_upload.js
@@ -1,9 +1,105 @@
 Template.attachmentsAddFromUpload.events({
+
+});
+
+Template.attachmentsAddFromUpload.onCreated(function() {
+  this.file = new ReactiveVar;
+  this.attachment = new ReactiveVar;
+})
+
+Template.attachmentsAddFromUpload.helpers({
+  onUploadSuccess() {
+    let template = Template.instance();
+    return function(fileObj) {
+      toastr.success(`The file ${fileObj.name} was downloaded and attached to this document.`);
+      template.attachment.set({
+      	fileId: fileObj._id,
+      	name: fileObj.name,
+      	type: fileObj.type
+      })
+    }
+  }
+})
+
+Template.smartInputFile.events({
   'change .btn-file :file': function(e, t) {
-  	$('.file-path').val(e.target.files[0].name);
+    let file = e.target.files[0];
+
+    t.file.set(file);
+    t.onFileSelected(file);
+  },
+
+  'click .btn-upload': function(e, t) {
+    let file = t.file.get();
+
+    t.onUploadBegin(file);
+    Files.insert(file, function(err, fileObj) {
+      if (err) return t.onUploadError(err);
+      return t.onUploadSuccess({
+        _id: fileObj._id,
+        name: fileObj.original.name,
+        type: fileObj.original.type,
+      });
+    });
   }
 });
 
-Template.attachmentsAddFromUpload.onCreated(function(){
+Template.smartInputFile.helpers({
+  btnFileText() {
+    return Template.instance().btnFileText();
+  },
+  btnUploadText() {
+    return Template.instance().btnUploadText.get();
+  },
+  fileNameText() {
+    return Template.instance().fileNameText();
+  },
+  isFileSelected() {
+    return Template.instance().isFileSelected();
+  }
+})
+
+Template.smartInputFile.onCreated(function() {
+  this.state = new ReactiveVar;
   this.file = new ReactiveVar;
+  this.btnUploadText = new ReactiveVar('Upload file');
+
+
+  // =======================================
+  // ========= Helper functions ============
+  // =======================================
+  this.isFileSelected = () => {
+    return this.file.get() !== undefined;
+  }
+
+  this.btnFileText = () => {
+    return this.isFileSelected() ? 'Change file...' : 'Browse file...';
+  }
+
+  this.fileNameText = () => {
+    return this.isFileSelected() ? this.file.get().name : '';
+  }
+
+  // =======================================
+  // ========= Callback functions ==========
+  // =======================================
+  this.onFileSelected = (file) => {
+    this.data.onFileSelected && this.data.onFileSelected(file);
+  }
+
+  this.onUploadBegin = (file) => {
+    this.btnUploadText.set('Uploading file...');
+    this.data.onUploadBegin && this.data.onUploadBegin(file);
+  }
+
+  this.onUploadError = (err) => {
+    console.error('onUploadError:', err);
+    this.btnUploadText.set('Upload file');
+    this.data.onUploadError && this.data.onUploadError(err);
+  }
+
+  this.onUploadSuccess = (fileObj) => {
+    this.btnUploadText.set('Upload file');
+    this.data.onUploadSuccess && this.data.onUploadSuccess(fileObj);
+  }
 })
