7974e10dfce3849cdc0034540c517963d9784d4f
lnmunhoz <ln.munhoz@gmail.com>
2016-02-18 18:10:44 -0200

    [file_preview] Now displaying images and data file should be a real fileObj

--- client/views/shared/file_preview/file_preview.js
+++ client/views/shared/file_preview/file_preview.js
@@ -1,27 +1,26 @@
 Template.filePreview.helpers({
   file() {
-	 return Template.instance().data.file;
+    return Template.instance().data.file;
   },
   classIconName() {
     return Template.instance().classIconName();
   }
 });
 
-Template.filePreview.onCreated(function(){
-
+Template.filePreview.onCreated(function() {
   this.classIconName = () => {
     let file = this.data.file || {};
-  	switch (file.type){
-  	  case 'application/pdf':
-  		return 'fa-file-pdf-o';
-  	  
-  	  case 'image/jpg':
-  	  case 'image/png':
-  	  case 'image/jpeg':
-  		return 'fa-file-image-o';
+    switch (file.type) {
+      case 'application/pdf':
+        return 'fa-file-pdf-o';
 
-  	  default:
-  	  	return 'fa-file-o';
-  	}
-  }
+      case 'image/jpg':
+      case 'image/png':
+      case 'image/jpeg':
+        return 'fa-file-image-o';
+
+      default:
+        return 'fa-file-o';
+    }
+  };
 });
