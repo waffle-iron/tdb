c383399214a206dad7437a8725b79aaaf5b39dcc
lnmunhoz <ln.munhoz@gmail.com>
2016-02-18 18:26:04 -0200

    [attach] Displaying spinnerWave while uploading the file

--- client/views/attachments/attachments_add_from_upload.js
+++ client/views/attachments/attachments_add_from_upload.js
@@ -10,13 +10,22 @@ Template.attachmentsAddFromUpload.helpers({
   fileObjFromUpload() {
     return Template.instance().fileObj.get();
   },
+  isUploading() {
+    return Template.instance().isUploading.get();
+  },
+  onUploadBegin() {
+    let t = Template.instance();
+    return function() {
+      t.isUploading.set(true);
+    };
+  },
   onUploadSuccess() {
-    let template = Template.instance();
+    let t = Template.instance();
     return function(fileObj) {
       toastr.success(`The file ${fileObj.original.name} was downloaded and attached to this document.`);
-      console.log(fileObj);
-      template.fileObj.set(fileObj);
-      template.attachment.set({
+      t.isUploading.set(false);
+      t.fileObj.set(fileObj);
+      t.attachment.set({
         fileId: fileObj._id,
         name: fileObj.original.name,
         type: fileObj.original.type
@@ -29,4 +38,5 @@ Template.attachmentsAddFromUpload.onCreated(function() {
   this.file = new ReactiveVar;
   this.fileObj = new ReactiveVar;
   this.attachment = new ReactiveVar;
+  this.isUploading = new ReactiveVar(false);
 });
