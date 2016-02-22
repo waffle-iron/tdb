14bdb1214962e8a8cb1f7f3aae555187c77cf43c
lnmunhoz <ln.munhoz@gmail.com>
2016-02-21 23:48:48 -0300

    Setup environment variables and configurations

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,3 +1,3 @@
 Meteor.startup(function() {
-  process.env.MAIL_URL = 'smtp://postmaster@sandbox2ef4e9bcc26c45db971f3530dda21c5f.mailgun.org:5552ef2fe1cf0f32211acec0b574b89c@smtp.mailgun.org:587';
+
 });
--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,5 +1,6 @@
 Cloudinary.config({
   cloud_name: Meteor.settings.public.CloudinaryName,
-  api_key: Meteor.settings.Cloudinary.accessKeyId,
-  api_secret: Meteor.settings.Cloudinary.secretAccessKey,
+  api_key: process.env.CLOUDINARY_ACCESS_KEY_ID,
+  api_secret: process.env.CLOUDINARY_SECRET_ACCESS_KEY,
 });
+
