dfad82a643182703ae72ddd40bc1121f0313a594
lnmunhoz <ln.munhoz@gmail.com>
2016-02-23 21:59:39 -0300

    [roles] Add, Edit & Delete Projecs

--- client/views/projects/projects_add/projects_add.js
+++ client/views/projects/projects_add/projects_add.js
@@ -5,7 +5,7 @@ AutoForm.hooks({
       FlowRouter.go('projects.dashboard');
     },
     onError(formType, error) {
-      toastr.success(error.toString(), 'Error');
+      toastr.error(error.toString(), 'Error');
     },
   }
 });
