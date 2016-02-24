f4183e35cc436bed7b0154a557b83cf48e2467e5
shact.developer@gmail.com <shact.developer@gmail.com>
2016-02-23 21:09:58 -0300

    transformedData on manageOrg

--- client/views/organizations/organizations_entry/manage_org_technologies.js
+++ client/views/organizations/organizations_entry/manage_org_technologies.js
@@ -1,11 +1,6 @@
 Template.manageOrgTechnologies.helpers({
   technologies() {
-    let results =  SearchSources.globalSearch.getData({
-      transform(matchText, regExp) {
-        return matchText.replace(regExp, '<b>$&</b>');
-      },
-      sort: {_score: -1}
-    });
+    let results =  SearchSources.globalSearch.getTransformedData();
     return results;
   },
   getOptions() {
