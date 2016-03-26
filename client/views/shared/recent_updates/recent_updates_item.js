859d605b777a63ba15fd89ac0533b313293fd35a
shact.developer@gmail.com <shact.developer@gmail.com>
2016-03-26 04:01:43 -0300

    recentUpdatesItem fix on text

--- client/views/shared/recent_updates/recent_updates_item.js
+++ client/views/shared/recent_updates/recent_updates_item.js
@@ -1,17 +1,22 @@
- Template.recentUpdatesItem.helpers({
-   color() {
-     return ColorMap.operations[this.operation] || ColorMap.operations.default;
-   },
-   icon() {
-     return Icons.collections[this.collection] || Icons.collections.default;
-   },
-   userIdentification() {
-     if (this.user()) {
-       return this.user().identification(['username', 'fullName', 'email']);
-     }
-     return 'Unknown';
-   },
-   routeName() {
-     return `${this.collection}.entry`;
-   }
- });
+Template.recentUpdatesItem.helpers({
+  getText() {
+    switch (Template.instance().data.operation) {
+      case 'update': return 'was updated by';
+      case 'insert': return 'was inserted by';
+      case 'delete': return 'was deleted by';
+      default: return 'unknown operation by';
+    }
+  },
+  color() {
+    return ColorMap.operations[this.operation] || ColorMap.operations.default;
+  },
+  icon() {
+    return Icons.collections[this.collection] || Icons.collections.default;
+  },
+  userIdentification() {
+    return this.user() && this.user().identification(['username', 'fullName', 'email']) || 'unknown';
+  },
+  routeName() {
+    return `${this.collection}.entry`;
+  }
+});
