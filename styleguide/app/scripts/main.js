b06d437ff3ef4b7d68d08cc2a281ba94a2859dba
Arthur Soares <arthursoares@gmail.com>
2016-03-31 22:10:39 +0200

    Added technologyCard, technologyDashboard, attachmentCard, attachmentDashboard, projectCard.

--- styleguide/app/scripts/main.js
+++ styleguide/app/scripts/main.js
@@ -2,11 +2,22 @@
 Handlebars.registerPartial('navbar', TDB.templates.navbar());
 Handlebars.registerPartial('footer', TDB.templates.footer());
 Handlebars.registerPartial('technologyCard', TDB.templates.technologyCard());
+Handlebars.registerPartial('projectCard', TDB.templates.projectCard());
+Handlebars.registerPartial('attachmentCard', TDB.templates.attachmentCard());
 
 
 // Register routes (hash, templateName, initCallback)
 TDBRouter.addRoute('', 'mainDashboard');
 TDBRouter.addRoute('#/technologiesDashboard','technologiesDashboard', () => {
+  $('.cards-box').masonry({
+    // options
+    columnWidth: '.cards-size',
+    gutter: 15,
+    percentPosition: true
+  });
+});
+
+TDBRouter.addRoute('#/attachmentsDashboard','attachmentsDashboard', () => {
   $('.cards-box').masonry({
     // options
     columnWidth: '.cards-size',
