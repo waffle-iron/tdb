ba96f46cbb187abd4e43cdc59389ef97bf18a964
lnmunhoz <ln.munhoz@gmail.com>
2016-04-04 13:10:51 -0300

    Update 2016-04-04T16:10:48.966Z

--- scripts/main.js
+++ scripts/main.js
@@ -50,11 +50,11 @@ TDBRouter.addRoute({
     technologies: [{
       name: 'Drone Delivery',
       description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-      src: '../images/technology.jpg'
+      src: 'images/technology.jpg'
     }, {
       name: 'Other cool technology',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-      src: '../images/organization.png'
+      src: 'images/organization.png'
     }],
     latestUpdates: [{
       object: 'Drone Delivery',
