68d4450aa11f987acf7555627eaed8216f268642
lnmunhoz <ln.munhoz@gmail.com>
2016-04-04 13:52:56 -0300

    Update 2016-04-04T16:52:55.053Z

--- scripts/main.js
+++ scripts/main.js
@@ -47,15 +47,7 @@ TDBRouter.addRoute({
   template: 'technologiesDashboard',
   onRendered: initMansory,
   context: {
-    technologies: [{
-      name: 'Drone Delivery',
-      description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-      src: 'images/technology.jpg'
-    }, {
-      name: 'Other cool technology',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-      src: 'images/organization.png'
-    }],
+    technologies: technologies,
     latestUpdates: [{
       object: 'Drone Delivery',
       type: 'gear',
@@ -72,15 +64,7 @@ TDBRouter.addRoute({
   template: 'technologyEdit',
   onRendered: initMansory,
   context: {
-    technologies: [{
-      name: 'Drone Delivery',
-      description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-      src: 'images/technology.jpg'
-    }, {
-      name: 'Other cool technology',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-      src: 'images/organization.png'
-    }],
+    technologies: technologies,
     latestUpdates: [{
       object: 'Drone Delivery',
       type: 'gear',
@@ -97,15 +81,7 @@ TDBRouter.addRoute({
   template: 'technologyEntry',
   onRendered: initMansory,
   context: {
-    technologies: [{
-      name: 'Drone Delivery',
-      description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-      src: 'images/technology.jpg'
-    }, {
-      name: 'Other cool technology',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-      src: 'images/organization.png'
-    }],
+    technologies: technologies,
     latestUpdates: [{
       object: 'Drone Delivery',
       type: 'gear',
@@ -129,15 +105,7 @@ TDBRouter.addRoute({
   template: 'technologyAdd',
   onRendered: initMansory,
   context: {
-    technologies: [{
-      name: 'Drone Delivery',
-      description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-      src: 'images/technology.jpg'
-    }, {
-      name: 'Other cool technology',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-      src: 'images/organization.png'
-    }],
+    technologies: technologies,
     latestUpdates: [{
       object: 'Drone Delivery',
       type: 'gear',
@@ -157,13 +125,7 @@ TDBRouter.addRoute({
   template: 'projectsDashboard',
   onRendered: initMansory,
   context: {
-    projects: [{
-      name: 'Futuro da Medicina',
-      evID: 'EV0091',
-      description: 'Futuro da Medicina is a content platform that is being developed for OneHealth since 2013. It started with some visualizations back in 2011. Our main contact is Dr. Sergio, CEO.',
-      src: 'images/project.png',
-      status: 'Active'
-    }],
+    projects: projects,
     latestUpdates: [{
       object: 'Futuro da Medicina',
       type: 'folder',
@@ -180,13 +142,7 @@ TDBRouter.addRoute({
   template: 'projectEntry',
   onRendered: initProjectEntry,
   context: {
-    projects: [{
-      name: 'Futuro da Medicina',
-      evID: 'EV0091',
-      description: 'Futuro da Medicina is a content platform that is being developed for OneHealth since 2013. It started with some visualizations back in 2011. Our main contact is Dr. Sergio, CEO.',
-      src: 'images/project.png',
-      status: 'Active'
-    }],
+    projects: projects,
     latestUpdates: [{
       object: 'Futuro da Medicina',
       type: 'folder',
@@ -203,60 +159,8 @@ TDBRouter.addRoute({
   onRendered: initProjectEntry,
   context: {
     collectionName: 'Taxonomy',
-    technologiesStash: [{
-      name: 'Drone Delivery',
-      description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-      src: 'images/technology.jpg'
-    }, {
-      name: 'Other cool technology',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-      src: 'images/organization.png'
-    }],
-    collection: [{
-      title: 'IT & Systems',
-      description: 'The capacity for being fast and efficient can mean success or failure in the field. With more resources requiring electricity to operate escalating demands should be quelled. Acquiring energy, transmitting energy, and effective use of energy gives an edge in deployed assets for their ability to remain self-sufficient and reduce expenditures on unnecessary losses.',
-      subCollection: [{
-        title: 'Distribution',
-        description: 'Means of distribution',
-        technologies: [{
-          name: 'Drone Delivery',
-          description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-          src: 'images/technology.jpg'
-        }, {
-          name: 'Other cool technology',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-          src: 'images/organization.png'
-        }]
-      }]
-    }, {
-      title: 'IT & Systems',
-      description: 'The capacity for being fast and efficient can mean success or failure in the field. With more resources requiring electricity to operate escalating demands should be quelled. Acquiring energy, transmitting energy, and effective use of energy gives an edge in deployed assets for their ability to remain self-sufficient and reduce expenditures on unnecessary losses.',
-      subCollection: [{
-        title: 'Distribution',
-        description: 'Means of distribution',
-        technologies: [{
-          name: 'Drone Delivery',
-          description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-          src: 'images/technology.jpg'
-        }, {
-          name: 'Other cool technology',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-          src: 'images/organization.png'
-        }]
-      }, {
-        title: 'Production',
-        description: 'Means of distribution',
-        technologies: [{
-          name: 'Drone Delivery',
-          description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-          src: 'images/technology.jpg'
-        }, {
-          name: 'Other cool technology',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-          src: 'images/organization.png'
-        }]
-      }]
-    }],
+    technologiesStash: technologies,
+    collection: projectCollection,
     latestUpdates: [{
       object: 'Futuro da Medicina',
       type: 'folder',
@@ -273,22 +177,7 @@ TDBRouter.addRoute({
   template: 'attachmentsDashboard',
   onRendered: initMansory,
   context: {
-    attachments: [{
-      name: 'Drone Delivery',
-      description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-      src: 'images/attachment.jpg',
-      type: 'Media'
-    }, {
-      name: 'Other cool technology',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-      src: 'images/organization.png',
-      type: 'Article'
-    }, {
-      name: 'Other cool technology',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
-      src: 'images/organization.png',
-      type: 'Article'
-    }],
+    attachments: attachments,
     latestUpdates: [{
       object: 'Drone Delivery',
       type: 'gear',
@@ -305,12 +194,7 @@ TDBRouter.addRoute({
   template: 'organizationsDashboard',
   onRendered: initMansory,
   context: {
-    organizations: [{
-      name: 'Tesla',
-      description: 'Tesla Motors, Inc. is an American automotive and energy storage company that designs, manufactures, and sells luxury electric cars, electric vehicle powertrain components, and battery products.',
-      src: 'images/organization.png',
-      type: 'Private'
-    }],
+    organizations: organizations,
     latestUpdates: [{
       object: 'Tesla',
       type: 'building',
