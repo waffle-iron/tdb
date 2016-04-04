ba30cbaac79664ae6e9141ba7e73af9db99f5ec5
Arthur Soares <arthursoares@gmail.com>
2016-04-04 12:35:16 +0200

    added attachmentDashboard. added latestUpdates. added ProjectDashboard

--- styleguide/app/scripts/main.js
+++ styleguide/app/scripts/main.js
@@ -5,6 +5,7 @@ Handlebars.registerPartial('technologyCard', TDB.templates.technologyCard);
 Handlebars.registerPartial('projectCard', TDB.templates.projectCard);
 Handlebars.registerPartial('attachmentCard', TDB.templates.attachmentCard);
 Handlebars.registerPartial('organizationCard', TDB.templates.organizationCard);
+Handlebars.registerPartial('latestUpdates', TDB.templates.latestUpdates);
 
 // Helpers
 function initMansory() {
@@ -23,7 +24,7 @@ TDBRouter.addRoute({
 });
 
 TDBRouter.addRoute({
-  path: '/technologiesDashboard',
+  path: '/technologies/',
   template: 'technologiesDashboard',
   onRendered: initMansory,
   context: {
@@ -35,13 +36,75 @@ TDBRouter.addRoute({
       name: 'Other cool technology',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
       src: '../images/organization.png'
+    }],
+    latestUpdates: [{
+    	object: 'Drone Delivery',
+    	type:'gear',
+    	url: '#/technologies/entry',
+    	action:'was created',
+    	author:'Arthur Soares',
+    	time:'5 min'
+    }]
+  }
+});
+
+TDBRouter.addRoute({
+  path: '/attachments/',
+  template: 'attachmentsDashboard',
+  onRendered: initMansory,
+  context: {
+    attachments: [{
+      name: 'Drone Delivery',
+      description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
+      src: '../images/attachment.jpg',
+      type: 'Media'
+    }, {
+      name: 'Other cool technology',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
+      src: '../images/organization.png',
+      type: 'Article'
+    }, {
+      name: 'Other cool technology',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
+      src: '../images/organization.png',
+      type: 'Article'
+    }],
+    latestUpdates: [{
+    	object: 'Drone Delivery',
+    	type:'gear',
+    	url: '#/technologies/entry',
+    	action:'was created',
+    	author:'Arthur Soares',
+    	time:'5 min'
+    }]
+  }
+});
+
+TDBRouter.addRoute({
+  path: '/projects/',
+  template: 'projectsDashboard',
+  onRendered: initMansory,
+  context: {
+    projects: [{
+      name: 'Futuro da Medicina',
+      evID: 'EV0091',
+      description: 'Futuro da Medicina is a content platform that is being developed for OneHealth since 2013. It started with some visualizations back in 2011. Our main contact is Dr. Sergio, CEO.',
+      src: '../images/project.png',
+      status: 'Active'
+    }],
+    latestUpdates: [{
+    	object: 'Futuro da Medicina',
+    	type:'folder',
+    	url: '#/projects/entry',
+    	action:'was created',
+    	author:'Arthur Soares',
+    	time:'5 min'
     }]
   }
 });
 
 // Finish this
 // TDBRouter.addRoute('technologiesDashboard', initMansory);
-// TDBRouter.addRoute('attachmentsDashboard', initMansory);
 // TDBRouter.addRoute('projectsDashboard', initMansory);
 // TDBRouter.addRoute('organizationsDashboard', initMansory);
 
