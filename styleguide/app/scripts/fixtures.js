101051d6c540268cbe0298363bc4aa4f36f5a2cc
Arthur Soares <arthursoares@gmail.com>
2016-04-04 23:00:34 +0200

    Better CSS classes for technology status. #17 #11

--- styleguide/app/scripts/fixtures.js
+++ styleguide/app/scripts/fixtures.js
@@ -1,7 +1,9 @@
 const technologies = [{
+  evID: 'T0091',
   name: 'Drone Delivery',
   description: 'Relatively cheap drones with advanced sensors and imaging capabilities are giving farmers new ways to increase yields and reduce crop damage',
-  src: 'images/technology.jpg'
+  src: 'images/technology.jpg',
+  status: 'Published'
 }, {
   name: 'Other cool technology',
   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat sapiente ipsa et voluptatum quibusdam temporibus veniam facilis praesentium nostrum, beatae sint quod earum quis, quos, delectus voluptatem sunt deleniti est.',
